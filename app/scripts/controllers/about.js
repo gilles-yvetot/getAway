3a395355baeb27f0887ab6700c1f1f1bd416ecf4
gilles-yvetot <g.yvetot@gmail.com>
2016-02-11 15:34:40 -0500

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name flightFareApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the flightFareApp
+ */
+angular.module('flightFareApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
