657c26cc95ecf6aacbd0c07ab3d4213d9e4eb802
gilles-yvetot <g.yvetot@gmail.com>
2016-02-19 17:41:52 -0500

    first call to QPX API

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,8 +7,15 @@
  * # MainCtrl
  * Controller of the flightFareApp
  */
+
+var apiKey=0;
+
+Date.prototype.toAPIdateString = function() {
+  return this.getFullYear()+"-"+("0" + (this.getMonth() + 1)).slice(-2)+"-"+("0" + this.getDate()).slice(-2);
+};
+
 angular.module('flightFareApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope,$http) {
     
     $scope.formData={
     	fromDt : new Date(),
@@ -20,6 +27,40 @@ angular.module('flightFareApp')
 
 	$scope.searchFlight = function(){
 
+    var params=
+      {
+        "request": {
+          "passengers": {
+            "adultCount": 1
+          },
+          "slice": [
+            {
+              "origin": $scope.formData.fromDest,
+              "destination": $scope.formData.toDest,
+              "date":  $scope.formData.fromDt.toAPIdateString()
+            },
+            {
+              "origin": $scope.formData.toDest,
+              "destination": $scope.formData.fromDest,
+              "date":  $scope.formData.toDt.toAPIdateString()
+            }
+          ]
+        }
+      }
+
+
+    $http.post("https://www.googleapis.com/qpxExpress/v1/trips/search?key="+apiKey,params )
+    .then(
+      function(res){
+        alert(JSON.stringify(res))
+        /*
+            {"data":{"kind":"qpxExpress#tripsSearch","trips":{"kind":"qpxexpress#tripOptions","requestId":"DkpFTCAMUuUlcSmCD0Np0B","data":{"kind":"qpxexpress#data"}}},"status":200,"config":{"method":"POST","transformRequest":[null],"transformResponse":[null],"url":"https://www.googleapis.com/qpxExpress/v1/trips/search?key=KEY HERE","data":{"request":{"passengers":{"adultCount":1},"slice":[{"origin":"MIA","destination":"BOS","date":"2016-02-19"},{"origin":"BOS","destination":"MIA","date":"2016-02-22"}]}},"headers":{"Accept":"application/json, text/plain, ","Content-Type":"application/json;charset=utf-8"}},"statusText":"OK"}"
+        */
+
+      },
+      function(res){
+        alert(JSON.stringify(res))}
+    )
 	}
 
   });
